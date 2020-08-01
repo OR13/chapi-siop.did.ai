5eb183a8d00582121b2b6288958ac935d377040c
Orie Steele <orie@transmute.industries>
2020-08-01 08:43:58 -0500

    :rocket:

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -3,14 +3,36 @@ import PropTypes from "prop-types";
 
 import Base from "../base/base";
 
-const { credentialHandlerPolyfill } = window;
+import * as chapi from "credential-handler-polyfill";
+import * as WebCredentialHandler from "web-credential-handler";
 
 export const Home = (props) => {
   React.useEffect(() => {
-    credentialHandlerPolyfill.loadOnce().then(() => {
-      console.log("CHAPI is loaded.");
-    });
-  }, []);
+    if (!window.__chapi__run__once) {
+      (async () => {
+        try {
+          await chapi.loadOnce();
+          const WALLET_LOCATION = window.origin;
+          const workerUrl = `${WALLET_LOCATION}/worker.html`;
+          const registration = await WebCredentialHandler.installHandler({
+            url: workerUrl,
+          });
+          await registration.credentialManager.hints.set("test", {
+            name: "TestUser",
+            enabledTypes: [
+              "VerifiablePresentation",
+              "VerifiableCredential",
+              "UniversityDegreeCredential",
+            ],
+          });
+          console.log("Wallet registered.");
+        } catch (e) {
+          console.error("Error in loadOnce:", e);
+        }
+      })();
+    }
+    window.__chapi__run__once = true;
+  });
 
   return <Base>hello</Base>;
 };
